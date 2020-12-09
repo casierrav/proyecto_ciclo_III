45bba6f28226f03a558d096d5a36ef69d10d760e
casierrav <jerarquia04@gmail.com>
2020-12-09 09:35:25 -0500

    Add FastAPI with right port to valid CORS origins

--- cajero_api/main.py
+++ cajero_api/main.py
@@ -19,7 +19,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080",
+    "http://localhost", "http://localhost:8080", "http://localhost:8081",
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
