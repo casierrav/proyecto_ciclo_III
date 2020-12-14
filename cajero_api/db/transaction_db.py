9efeb8280ff9c3885b77a570f475330d8eca2e27
casierrav <jerarquia04@gmail.com>
2020-12-14 09:21:51 -0500

    Add changes for Database layer connection

--- cajero_api/db/transaction_db.py
+++ cajero_api/db/transaction_db.py
@@ -1,3 +1,20 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+import datetime
+
+from db.db_connection import Base, engine
+
+class TransactionInDB(Base):
+    __tablename__ = "transactions"
+
+    id              = Column(Integer, primary_key=True, autoincrement=True)
+    username        = Column(String, ForeignKey("users.username"))
+    date            = Column(DateTime, default=datetime.datetime.utcnow)
+    value           = Column(Integer)
+    actual_balance  = Column(Integer)
+
+Base.metadata.create_all(bind=engine)
+
+'''
 from datetime import datetime
 from pydantic import BaseModel
 
@@ -19,3 +36,4 @@ def save_transaction(transaction_in_db: TransactionInDB):
     transaction_in_db.id_transaction = generator["id"]
     database_transactions.append(transaction_in_db)
     return transaction_in_db
+'''
